729b004148c157f8b1f72a448e292c20a20939ce
samir@xenon <samir@xenon>
2021-10-30 12:58:29 -0500

    completed hoot and toot

--- hoot_toot_braid50/hoot_toot_braid50.ino
+++ hoot_toot_braid50/hoot_toot_braid50.ino
@@ -8,23 +8,27 @@
 #define NUM_LEDS 50
 #define DATA_PIN 11
 #define BRIGHTNESS 200
+#define FIRST_LED 23
+#define TOOT_START 28 // green
+#define TOOT_END 32 // green
+#define HOOT_START 42 // blue
+#define HOOT_END 46 // blue
 
 CRGB leds[NUM_LEDS];
 
-unsigned short idx = 0;
+unsigned short idx = FIRST_LED;
 unsigned short prev = 0;
 
 TBlendType    currentBlending;
-CRGBPalette16 currentPalette;
 //extern CRGBPalette16 myRedWhiteBluePalette;
-extern const TProgmemPalette16 SirenPalette PROGMEM;
+extern const TProgmemPalette16 HootPalette PROGMEM; // blue
+extern const TProgmemPalette16 TootPalette PROGMEM; // green
 
 
 void setup() {
   delay( 2000 ); // power-up safety delay
   // put your setup code here, to run once:
   FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS);
-  currentPalette = SirenPalette;
   currentBlending = LINEARBLEND;
 #ifdef DEBUG
   Serial.begin(9600);
@@ -34,85 +38,84 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  cp_test();
+  hoot_toot();
 }
 
-void cp_test()
+void hoot_toot()
 {
-  // only lighting up some of the LEDS for the fire engine costume
   ++idx;
   idx &= 0xff;
-  leds[1] = ColorFromPalette( currentPalette, idx + 0, BRIGHTNESS, currentBlending);
-  leds[2] = ColorFromPalette( currentPalette, (idx + 64) & 0xff, BRIGHTNESS, currentBlending);
 
-  // phase difference gives a kind of circular effect
-  leds[8] = ColorFromPalette( currentPalette, idx, BRIGHTNESS, currentBlending);
-  leds[9] = ColorFromPalette( currentPalette, (idx + 8) & 0xff, BRIGHTNESS, currentBlending);
-  leds[10] = ColorFromPalette( currentPalette, (idx + 16) & 0xff, BRIGHTNESS, currentBlending);
-  leds[11] = ColorFromPalette( currentPalette, (idx + 32) & 0xff, BRIGHTNESS, currentBlending);
-
-  leds[30] = ColorFromPalette( currentPalette, idx, BRIGHTNESS, currentBlending);
-  leds[31] = ColorFromPalette( currentPalette, (idx + 64) & 0xff, BRIGHTNESS, currentBlending);
-
-  // reverse indexes so they go from front to back, du to circular mounting
-  leds[47] = ColorFromPalette( currentPalette, idx, BRIGHTNESS, currentBlending);
-  leds[46] = ColorFromPalette( currentPalette, (idx + 8) & 0xff, BRIGHTNESS, currentBlending);
-  leds[45] = ColorFromPalette( currentPalette, (idx + 16) & 0xff, BRIGHTNESS, currentBlending);
-  leds[44] = ColorFromPalette( currentPalette, (idx + 32) & 0xff, BRIGHTNESS, currentBlending);
-
-
-  // crazy strobe effect
-  /*
-  if ( idx <= 50 ) {
-    if ( idx > 0 ) {
-      leds[idx-1]  = CRGB::Black;
+  for ( int i = FIRST_LED; i < NUM_LEDS; ++i ) {
+    if ( i >= HOOT_START && i <= HOOT_END ) {
+      leds[i] = ColorFromPalette( HootPalette, idx + 0, BRIGHTNESS, currentBlending);
     }
-    if ( idx < 50 ) {
-      leds[idx]  = CRGB::White;
+    else if ( i >= TOOT_START && i <= TOOT_END ) {
+      leds[i] = ColorFromPalette( TootPalette, idx + 0, BRIGHTNESS, currentBlending);
     }
-  }  
-  */
+    else {
+      // for the non-hoot/toot alternate the remaining leds
+      int oddon = idx & 0x40 ? 1 : 0;
+      leds[i] = ( i % 2 == oddon) ? CRGB::Black : CRGB::DeepPink;
+    }
+  }
   FastLED.show();
   FastLED.delay(10);
 }
 
-void test()
+void count_leds()
 {
-
-  unsigned short curr = idx < NUM_LEDS ? idx : 2*NUM_LEDS-idx;
-  leds[prev] = CRGB(0,0,0);
-  leds[curr] = CRGB(100,0,0);
-  prev = curr;
-  
+  leds[idx] = CRGB::Black;
+  ++idx;
+  if ( idx > NUM_LEDS ) {
+    idx = FIRST_LED;
+  }
+  leds[idx] = CRGB::Red;
   FastLED.show();
-  if (++idx >= 2*NUM_LEDS) { idx = 0; }
-  delay(50);
-}
+  FastLED.delay(10);
 
+}
 
 // This example shows how to set up a static color palette
 // which is stored in PROGMEM (flash), which is almost always more
 // plentiful than RAM.  A static PROGMEM palette like this
 // takes up 64 bytes of flash.
-const TProgmemPalette16 SirenPalette PROGMEM =
+const TProgmemPalette16 HootPalette PROGMEM =
 {
-    CRGB::Black,
-    CRGB::DarkRed,
-    CRGB::Red,
-    CRGB::DarkRed,
-    
-    CRGB::Black,
-    CRGB::DarkBlue,
+    CRGB::Blue,
+    CRGB::Blue,
+    CRGB::Blue,
     CRGB::Blue,
     CRGB::DarkBlue,
-    
+    CRGB::DarkBlue,
+    CRGB::DarkBlue,
     CRGB::Black,
-    CRGB::DarkRed,
-    CRGB::Red,
-    CRGB::DarkRed,
-    
     CRGB::Black,
     CRGB::DarkBlue,
+    CRGB::DarkBlue,
+    CRGB::DarkBlue,
     CRGB::Blue,
-    CRGB::DarkBlue
+    CRGB::Blue,
+    CRGB::Blue,
+    CRGB::Blue
+};
+
+const TProgmemPalette16 TootPalette PROGMEM =
+{
+    CRGB::Green,
+    CRGB::Green,
+    CRGB::Green,
+    CRGB::Green,
+    CRGB::DarkGreen,
+    CRGB::DarkGreen,
+    CRGB::DarkGreen,
+    CRGB::Black,
+    CRGB::Black,
+    CRGB::DarkGreen,
+    CRGB::DarkGreen,
+    CRGB::DarkGreen,
+    CRGB::Green,
+    CRGB::Green,
+    CRGB::Green,
+    CRGB::Green
 };
